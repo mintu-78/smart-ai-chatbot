8cae81e741ce5aa5603d78987be5a6dc71f2278a
Mintu <pmk80028@gmail.com>
2026-05-08 08:07:23 +0530

    Setup chatbot UI and connect Gemini API endpoint

---
+++ script.js
@@ -0,0 +1,50 @@
+import { apiKey} from "./config.js"
+
+const chatBox = document.getElementById("chat-box");
+const userInput = document.getElementById("user-input");
+const sendBtn =  document.getElementById("send-btn");
+
+
+function addMessage(message,className){
+    const msgDiv = document.createElement("div");
+    msgDiv.classList.add("message",className);
+    msgDiv.textContent = message;
+    chatBox.appendChild(msgDiv);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+
+function showTyping(){
+    const typingDiv = document.createElement("div");
+    typingDiv.classList.add("message","bot-message");
+    typingDiv.textContent = "AI is typing...";
+    chatBox.appendChild(typingDiv);
+    chatBox.scrollTop = chatBox.scrollHeight;
+    return typingDiv;
+}
+
+
+async function getBotReplay (userMessage){
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`;
+    
+}
+
+
+sendBtn.onclick = async() => {
+    const message = userInput.value.trim();
+    if(message === "") return;
+    addMessage(message, "user-message");
+    userInput.value = "";
+
+    const typingDiv = showTyping();
+
+    const botReplay = await getBotReplay(message);
+    typingDiv.remove();
+    addMessage(botReplay,"bot-message");
+    localStorage.setItem("ChatHistory",chatBox.innerHTML);
+
+}
+
+userInput.addEventListener("keypress",(e) =>{
+    if(e.key === "Enter") sendBtn.click();
+})
